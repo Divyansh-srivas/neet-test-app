73c9d07ebf91089e62f3da8375bd55d58f0e5c8b
Divyansh Srivastava <srivastavamaanav48@gmail.com>
2026-07-29 21:27:23 +0530

    Cache bust for production deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+console.log("Neogravix Web App - v1.0.1 (Cache Busting)");
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
