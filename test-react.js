543f31b1be7e9a1f632e127690648cdac8714f41
divyansh34 <srivastavamaanav48@gmail.com>
2026-09-21 00:08:21 +0530

    chore: remove test scripts

--- test-react.js
+++
@@ -1,5 +0,0 @@
-import React, { useState } from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import { JSDOM } from 'jsdom';
-
-// We don't have testing-library installed. I will just do a tiny vanilla React test.
